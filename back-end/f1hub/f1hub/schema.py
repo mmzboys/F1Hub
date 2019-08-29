c50f75b9dea57a0a75db3523d3cee5d65b8c204a
haelmorn <haelmorn@gmail.com>
2019-08-29 16:31:51 +0200

    Added seasons, the last app

--- back-end/f1hub/f1hub/schema.py
+++ back-end/f1hub/f1hub/schema.py
@@ -12,11 +12,12 @@ import driverstandings.schema
 import laptimes.schema
 import pitstops.schema
 import qualifying.schema
+import seasons.schema
 
 
 class Query(drivers.schema.Query, results.schema.Query, constructors.schema.Query, races.schema.Query, status.schema.Query, circuits.schema.Query,\
     constructorresults.schema.Query, constructorstandings.schema.Query, driverstandings.schema.Query, laptimes.schema.Query, pitstops.schema.Query,\
-    qualifying.schema.Query ,graphene.ObjectType):
+    qualifying.schema.Query, seasons.schema.Query, graphene.ObjectType):
     pass
 
 
